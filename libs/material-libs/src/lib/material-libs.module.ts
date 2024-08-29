4579fe3e2c60698407f81c877e0b2aabf57aae86
MarcosGui <mgui@li-jiang.com>
2024-08-29 15:17:46 +0800

    feat: update some material modules

--- libs/material-libs/src/lib/material-libs.module.ts
+++ libs/material-libs/src/lib/material-libs.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
+import {
+  MatButtonModule,
+} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+const MAT_MODULES = [MatButtonModule, MatCardModule];
 
 @NgModule({
   imports: [CommonModule],
-  exports: [MatButtonModule],
+  exports: [...MAT_MODULES],
 })
 export class MaterialLibsModule {}
